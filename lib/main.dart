c731df8b2778924257d64a6f1e391e08a3df5f61
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2023-12-05 17:36:50 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(const AppWidget(title: "Hello World'"));
+}
+
+class AppWidget extends StatelessWidget {
+  final String title;
+
+  const AppWidget({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Text(
+      title,
+      textDirection: TextDirection.ltr,
+      style: const TextStyle(color: Colors.red, fontSize: 50),
+    ));
+  }
+}
