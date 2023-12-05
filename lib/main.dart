cb1320872808c2b19e1dc9e22e3d8c27f7c922cc
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2023-12-05 17:38:31 -0300

    fixed Hello World

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main(List<String> args) {
-  runApp(const AppWidget(title: "Hello World'"));
+  runApp(const AppWidget(title: "Hello World"));
 }
 
 class AppWidget extends StatelessWidget {
